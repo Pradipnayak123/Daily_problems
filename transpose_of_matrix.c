1d075cbec3c87e3046d2a8bbdd1e27f69f238092
Pradip Nayak <pradipn962@gmail.com>
2026-04-08 23:31:21 +0530

    Day-28 Transpose of a Matrix

---
+++ transpose_of_matrix.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter The Number of coloum/row:");
+    scanf("%d",&n);
+    
+    int arr[n][n];
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+         scanf("%d",&arr[i][j]);
+      }
+      
+    }
+    
+for (int i = 0; i < n; i++)
+{
+   for (int j= 0; j < n; j++)
+   {
+      int temp = arr[i][j];
+      arr[i][j] = arr[j][i];
+      arr[j][i] = temp;
+   }
+   
+}
+for (int i = 0; i < n; i++)
+{
+   for (int j= 0; j < n; j++)
+   {
+      int temp = arr[i][j];
+      arr[i][j] = arr[j][i];
+      arr[j][i] = temp;
+   }
+   
+}
+for (int i = 0; i < n; i++)
+{
+   for (int j = 0; j < n; j++)
+   {
+      printf("%d",arr[i][j]);
+      
+   }
+   printf("\n");
+   
+}
+
+    return 0;
+}
