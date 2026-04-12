ec863264366d8ebd75ae55e4e4828b02ebc4061e
Pradip Nayak <pradipn962@gmail.com>
2026-04-12 23:23:08 +0530

    Day - 31 Bubble Sort , Which is optimize Using bool Flag

---
+++ special_bubble_sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    int arr[50];
+    int n;
+    printf("Enter The Array Size:");
+    scanf("%d",&n);
+    printf("Enter Array Element:\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    printf("Original Array:\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    //Bubble Sort
+    for(int i=0;i<n-1;i++){
+        
+        bool flag =false;//
+        for(int j=0;j<=n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp =arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                flag = false;
+            }
+            
+        }
+        if(flag==true)break;
+    }
+    printf("\n");
+    
+     printf("sorted  Array:\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    
+    return 0;
+}
