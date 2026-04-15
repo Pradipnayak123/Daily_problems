7022620dee278775caa3e77a36fdda13c1f1a0ba
Pradip Nayak <pradipn962@gmail.com>
2026-04-15 23:57:30 +0530

    Day - 34 Sorting using Insertion Sorting Algo

---
+++ insertion_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+    int arr[50];
+    int n;
+    printf("Enter The Array Size:");
+    scanf("%d",&n);
+    printf("Enter Array Element:\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    printf("Original Array:\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    //Insertion Sort
+    for(int i = 1;i<=n-1;i++){
+        int j = i;
+        while (j>=1 && arr[j]<arr[j-1])
+        {
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+            j--;
+        }
+        
+    }
+    
+    printf("\n");
+    
+     printf("sorted  Array:\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    
+    return 0;
+}
