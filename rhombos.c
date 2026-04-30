7d20c7e1e7013985ebee6ac5953499257d4ae0fe
Pradip Nayak <pradipn962@gmail.com>
2026-04-30 23:54:36 +0530

    Day - 40

---
+++ rhombos.c
@@ -0,0 +1,28 @@
+.c
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter a Number :");
+    scanf("%d",&n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n-i; j++)
+        {
+            printf(" "); // for space
+        }
+            for (int k = 1; k <=n; k++)
+            {
+                printf("*");
+                
+            }
+            
+        
+        printf("\n");
+        
+        
+    }
+    
+    
+    return 0;
+}
