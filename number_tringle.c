643244f052175901e9f21995e7585ba71e3ed211
Pradip Nayak <pradipn962@gmail.com>
2026-05-08 23:30:55 +0530

    Day - 46 Number Triangla

---
+++ number_tringle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int rows,result;
+    printf("Enter  Number of Rows:");
+    scanf("%d",&rows);
+    for (int i = 1; i <= rows; i++)// Noof rows --> i;
+    {
+        for (int j = 1; j <= i; j++)// no of coloums --> j;
+        {
+            printf("%d",j);
+        
+        
+            
+        }
+        printf("\n");
+        
+        
+    }
+    
+    
+    
+    return 0;
+}
